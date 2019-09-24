afe8ef7daa4aff41752ae3212183543005d19275
Marc Müller <marc.mueller@4tecture.ch>
2019-09-24 08:43:06 +0200

    graceterm

--- Greeter.Service/Greeter.Service/Startup.cs
+++ Greeter.Service/Greeter.Service/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Graceterm;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -40,6 +41,8 @@ namespace Greeter.Service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseGraceterm(options => options.IgnorePath("/api/status"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
