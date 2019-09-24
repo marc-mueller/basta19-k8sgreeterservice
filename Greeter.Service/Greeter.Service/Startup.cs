7a46a754a2b0d0b11290867bd7edc230ea5c3e97
Marc Müller <marc.mueller@4tecture.ch>
2019-09-24 10:45:17 +0200

    remove graceterm

--- Greeter.Service/Greeter.Service/Startup.cs
+++ Greeter.Service/Greeter.Service/Startup.cs
@@ -41,8 +41,6 @@ namespace Greeter.Service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseGraceterm(options => options.IgnorePath("/api/status"));
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
